bd235366c0767bd2cae5ee3e9eabc2a1e4b4b11a
ashiqur-r-biplop <biplops22@gmail.com>
2024-03-10 01:16:58 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("card-1").addEventListener("mouseover", () => {
+  const card = document.getElementById("card-1");
+  card.style.transform = "rotateY(180deg)";
+});
+document.getElementById("card-1").addEventListener("mouseleave", () => {
+  const card = document.getElementById("card-1");
+  card.style.transform = "rotateY(0deg)";
+  console.log("done");
+});
+document.getElementById("card-2").addEventListener("mouseover", () => {
+  const card = document.getElementById("card-2");
+  card.style.transform = "rotateY(180deg)";
+});
+document.getElementById("card-2").addEventListener("mouseleave", () => {
+  const card = document.getElementById("card-2");
+  card.style.transform = "rotateY(0deg)";
+  console.log("done");
+});
+document.getElementById("card-3").addEventListener("mouseover", () => {
+  const card = document.getElementById("card-3");
+  card.style.transform = "rotateY(180deg)";
+});
+document.getElementById("card-3").addEventListener("mouseleave", () => {
+  const card = document.getElementById("card-3");
+  card.style.transform = "rotateY(0deg)";
+  console.log("done");
+});
