289a822608f00c209e48f64db227d1c880735a99
ashiqur-r-biplop <biplops22@gmail.com>
2024-03-10 11:46:33 +0600

    new section add

--- script.js
+++ script.js
@@ -25,3 +25,48 @@ document.getElementById("card-3").addEventListener("mouseleave", () => {
   card.style.transform = "rotateY(0deg)";
   console.log("done");
 });
+document.getElementById("Emphasize-card-1").addEventListener("mouseover", () => {
+  const card = document.getElementById("Emphasize-card-1");
+  card.style.transform = "rotateY(180deg)";
+});
+document.getElementById("Emphasize-card-1").addEventListener("mouseleave", () => {
+  const card = document.getElementById("Emphasize-card-1");
+  card.style.transform = "rotateY(0deg)";
+  console.log("done");
+});
+document.getElementById("Emphasize-card-2").addEventListener("mouseover", () => {
+  const card = document.getElementById("Emphasize-card-2");
+  card.style.transform = "rotateY(180deg)";
+});
+document.getElementById("Emphasize-card-2").addEventListener("mouseleave", () => {
+  const card = document.getElementById("Emphasize-card-2");
+  card.style.transform = "rotateY(0deg)";
+  console.log("done");
+});
+document.getElementById("Emphasize-card-3").addEventListener("mouseover", () => {
+  const card = document.getElementById("Emphasize-card-3");
+  card.style.transform = "rotateY(180deg)";
+});
+document.getElementById("Emphasize-card-3").addEventListener("mouseleave", () => {
+  const card = document.getElementById("Emphasize-card-3");
+  card.style.transform = "rotateY(0deg)";
+  console.log("done");
+});
+document.getElementById("Emphasize-card-4").addEventListener("mouseover", () => {
+  const card = document.getElementById("Emphasize-card-4");
+  card.style.transform = "rotateY(180deg)";
+});
+document.getElementById("Emphasize-card-4").addEventListener("mouseleave", () => {
+  const card = document.getElementById("Emphasize-card-4");
+  card.style.transform = "rotateY(0deg)";
+  console.log("done");
+});
+document.getElementById("Emphasize-card-5").addEventListener("mouseover", () => {
+  const card = document.getElementById("Emphasize-card-5");
+  card.style.transform = "rotateY(180deg)";
+});
+document.getElementById("Emphasize-card-5").addEventListener("mouseleave", () => {
+  const card = document.getElementById("Emphasize-card-5");
+  card.style.transform = "rotateY(0deg)";
+  console.log("done");
+});
