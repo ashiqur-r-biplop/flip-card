a617a831c4972fb5aa22298d4f849e80e4d000c5
ashiqur-r-biplop <biplops22@gmail.com>
2024-03-10 19:26:18 +0600

    added zoom in effect

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// addressing Your Concerns js start
 document.getElementById("card-1").addEventListener("mouseover", () => {
   const card = document.getElementById("card-1");
   card.style.transform = "rotateY(180deg)";
@@ -5,7 +6,6 @@ document.getElementById("card-1").addEventListener("mouseover", () => {
 document.getElementById("card-1").addEventListener("mouseleave", () => {
   const card = document.getElementById("card-1");
   card.style.transform = "rotateY(0deg)";
-  console.log("done");
 });
 document.getElementById("card-2").addEventListener("mouseover", () => {
   const card = document.getElementById("card-2");
@@ -14,7 +14,6 @@ document.getElementById("card-2").addEventListener("mouseover", () => {
 document.getElementById("card-2").addEventListener("mouseleave", () => {
   const card = document.getElementById("card-2");
   card.style.transform = "rotateY(0deg)";
-  console.log("done");
 });
 document.getElementById("card-3").addEventListener("mouseover", () => {
   const card = document.getElementById("card-3");
@@ -23,50 +22,80 @@ document.getElementById("card-3").addEventListener("mouseover", () => {
 document.getElementById("card-3").addEventListener("mouseleave", () => {
   const card = document.getElementById("card-3");
   card.style.transform = "rotateY(0deg)";
-  console.log("done");
 });
-document.getElementById("Emphasize-card-1").addEventListener("mouseover", () => {
-  const card = document.getElementById("Emphasize-card-1");
-  card.style.transform = "rotateY(180deg)";
+// addressing Your Concerns js end
+
+// Emphasize Unique Benefits js start
+document
+  .getElementById("Emphasize-card-1")
+  .addEventListener("mouseover", () => {
+    const card = document.getElementById("Emphasize-card-1");
+    card.style.transform = "rotateY(180deg)";
+  });
+document
+  .getElementById("Emphasize-card-1")
+  .addEventListener("mouseleave", () => {
+    const card = document.getElementById("Emphasize-card-1");
+    card.style.transform = "rotateY(0deg)";
+  });
+document
+  .getElementById("Emphasize-card-2")
+  .addEventListener("mouseover", () => {
+    const card = document.getElementById("Emphasize-card-2");
+    card.style.transform = "rotateY(180deg)";
+  });
+document
+  .getElementById("Emphasize-card-2")
+  .addEventListener("mouseleave", () => {
+    const card = document.getElementById("Emphasize-card-2");
+    card.style.transform = "rotateY(0deg)";
+  });
+document
+  .getElementById("Emphasize-card-3")
+  .addEventListener("mouseover", () => {
+    const card = document.getElementById("Emphasize-card-3");
+    card.style.transform = "rotateY(180deg)";
+  });
+document
+  .getElementById("Emphasize-card-3")
+  .addEventListener("mouseleave", () => {
+    const card = document.getElementById("Emphasize-card-3");
+    card.style.transform = "rotateY(0deg)";
+  });
+document
+  .getElementById("Emphasize-card-4")
+  .addEventListener("mouseover", () => {
+    const card = document.getElementById("Emphasize-card-4");
+    card.style.transform = "rotateY(180deg)";
+  });
+document
+  .getElementById("Emphasize-card-4")
+  .addEventListener("mouseleave", () => {
+    const card = document.getElementById("Emphasize-card-4");
+    card.style.transform = "rotateY(0deg)";
+  });
+document
+  .getElementById("Emphasize-card-5")
+  .addEventListener("mouseover", () => {
+    const card = document.getElementById("Emphasize-card-5");
+    card.style.transform = "rotateY(180deg)";
+  });
+document
+  .getElementById("Emphasize-card-5")
+  .addEventListener("mouseleave", () => {
+    const card = document.getElementById("Emphasize-card-5");
+    card.style.transform = "rotateY(0deg)";
+  });
+// Emphasize Unique Benefits js end
+
+// overlay card image zoom in effect
+document.getElementById("overLay-card").addEventListener("mouseenter", () => {
+  const card = document.getElementById("overlay-layer");
+  card.style.transform = "scale(100%)";
+  card.style.transition = "transform 1s";
 });
-document.getElementById("Emphasize-card-1").addEventListener("mouseleave", () => {
-  const card = document.getElementById("Emphasize-card-1");
-  card.style.transform = "rotateY(0deg)";
-  console.log("done");
-});
-document.getElementById("Emphasize-card-2").addEventListener("mouseover", () => {
-  const card = document.getElementById("Emphasize-card-2");
-  card.style.transform = "rotateY(180deg)";
-});
-document.getElementById("Emphasize-card-2").addEventListener("mouseleave", () => {
-  const card = document.getElementById("Emphasize-card-2");
-  card.style.transform = "rotateY(0deg)";
-  console.log("done");
-});
-document.getElementById("Emphasize-card-3").addEventListener("mouseover", () => {
-  const card = document.getElementById("Emphasize-card-3");
-  card.style.transform = "rotateY(180deg)";
-});
-document.getElementById("Emphasize-card-3").addEventListener("mouseleave", () => {
-  const card = document.getElementById("Emphasize-card-3");
-  card.style.transform = "rotateY(0deg)";
-  console.log("done");
-});
-document.getElementById("Emphasize-card-4").addEventListener("mouseover", () => {
-  const card = document.getElementById("Emphasize-card-4");
-  card.style.transform = "rotateY(180deg)";
-});
-document.getElementById("Emphasize-card-4").addEventListener("mouseleave", () => {
-  const card = document.getElementById("Emphasize-card-4");
-  card.style.transform = "rotateY(0deg)";
-  console.log("done");
-});
-document.getElementById("Emphasize-card-5").addEventListener("mouseover", () => {
-  const card = document.getElementById("Emphasize-card-5");
-  card.style.transform = "rotateY(180deg)";
-});
-document.getElementById("Emphasize-card-5").addEventListener("mouseleave", () => {
-  const card = document.getElementById("Emphasize-card-5");
-  card.style.transform = "rotateY(0deg)";
-  console.log("done");
+document.getElementById("overLay-card").addEventListener("mouseleave", () => {
+  const card = document.getElementById("overlay-layer");
+  card.style.transform = "scale(-0%)";
+  card.style.transition = "transform 1s";
 });
